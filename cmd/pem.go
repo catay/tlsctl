749e60f2ffb8a8be2374344c4fb345500a00ae7b
Steven Mertens <steven.mertens@catay.be>
2026-02-02 22:59:50 +0100

    feat: add --insecure flag to skip certificate verification

--- cmd/pem.go
+++ cmd/pem.go
@@ -26,5 +26,5 @@ func runPem(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	return outputChain(chainInfo, pemOutputFormat)
+	return outputChain(chainInfo, pemOutputFormat, false)
 }
